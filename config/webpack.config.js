ba22efda74bf3982b035753d549c94b61dee76b4
Aldresus <hugo.champy@viacesi.fr>
2023-10-14 11:13:39 +0200

    v 1.0.0

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,8 +8,8 @@ const PATHS = require('./paths');
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
-    app: PATHS.src + '/app.js',
-    background: PATHS.src + '/background.js',
+    // app: PATHS.src + '/app.js',
+    // background: PATHS.src + '/background.js',
   },
 });
 
